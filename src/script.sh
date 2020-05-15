a2a30c5c42a71e145aa1fd7999a182e68374fdf5
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-05-15 02:19:42 +0000

    first commit

--- src/script.sh
+++ src/script.sh
@@ -3,3 +3,41 @@
 # script.sh
 #
 #
+init() {
+	echo "init"
+}
+
+fetch() {
+	echo "fetch"
+}
+
+link() {
+	echo "link"
+}
+run() {
+	echo "run"
+}
+print_help() {
+	echo "print help"
+}
+
+case "${1:-}" in
+	"init" )
+		init
+	;;
+	"fetch" )
+		fetch
+	;;
+	"link" )
+		link
+	;;
+	"run" )
+		run
+	;;
+	*)
+		print_help
+	;;
+esac
+
+trap '' EXIT
+exit 0
