02c62431c2f11dac823226020ac5d1060424d8f1
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-05-16 01:02:43 +0000

    initialize function

--- src/script.sh
+++ src/script.sh
@@ -3,12 +3,27 @@
 # script.sh
 #
 #
+PATH_PROJECT=~/fs/
 init() {
 	echo "init"
+	mkdir -p $PATH_PROJECT
+
 }
 
 fetch() {
 	echo "fetch"
+	mkdir -p $PATH_PROJECT${2}
+	cd $PATH_PROJECT${2}
+	case "${1:-}" in
+		"docker" )
+			sudo docker create --name ${2} ${2}
+			sudo docker export --output="${2}_fs.tar" ${2}
+			sudo docker rm -f ${2}
+			sudo tar -xf "${2}_fs.tar"
+			sudo rm "${2}_fs.tar"
+			;;
+	esac
+	echo "fs fetched from ${1}"
 }
 
 link() {
@@ -16,23 +31,30 @@ link() {
 }
 run() {
 	echo "run"
+	cmd=${2}" "${3}
+	sudo systemd-nspawn --machine ${1} --directory $PATH_PROJECT${1} $cmd
 }
 print_help() {
-	echo "print help"
+	printf "Usage : ./script <Operation> <Param>
+	Operations :
+		init	: init ....
+		fetch   : fetch fs from docker or .....
+		link    : link
+		run     : run a command "
 }
 
 case "${1:-}" in
 	"init" )
-		init
+		init ${2}
 	;;
 	"fetch" )
-		fetch
+		fetch ${2} ${3}
 	;;
 	"link" )
 		link
 	;;
 	"run" )
-		run
+		run ${2} ${3} ${4}
 	;;
 	*)
 		print_help
